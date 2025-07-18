b274458d603fbac66b1a54cfb8502fddd8d397ae
mubina <mubina>
2025-07-18 19:37:23 +0500

    Vazifaa

---
+++ js/app.js
@@ -0,0 +1,94 @@
+// 1 misol
+
+// let son = (prompt("Haqiqiy son kiriting:"));
+
+
+// let pastga = Math.floor(son);
+
+// let yuqoriga = Math.ceil(son);
+
+
+// let yaqin = Math.round(son);
+
+// console.log(`floor ${pastga}`);
+// console.log(`seil ${yuqoriga}`);
+// console.log(`round ${yaqin}`);
+
+// 2 misol
+
+
+// let num = Math.floor(Math.random() * 11);
+// console.log(num);
+
+// 3 misol
+
+
+// let a;
+// let b = null;
+
+// console.log("a:", a); 
+// console.log("b:", b); 
+
+// 4 misol
+
+// let sanoq = 0;
+
+// while (sanoq < 10) {
+//     let son = Math.floor(Math.random() * 101); 
+//     console.log(son);
+//     sanoq = sanoq + 1;
+// }
+
+// 5 misol
+
+
+// let son = prompt("Biror son kiriting:")
+// let kvadrat = Math.pow(son, 2);
+
+// console.log(`${son} ning kvadrati:`, kvadrat);
+
+
+// 6 misol
+
+
+// let son = 1;
+
+// while (son <= 20) {
+//     if (son % 2 === 0) {
+//         son = son + 1; 
+//         continue;
+//     }
+//     console.log(son);
+//     son = son + 1;
+// }
+
+// 7 misol
+
+// let randomSon = Math.floor(Math.random() * 10) + 1; 
+// let user;
+
+// do {
+//     user = +prompt("1 dan 10 gacha son toping:");
+//     if (user !== randomSon){
+//         console.log("Xato. Yana kiriting.")
+//     }
+// } while (user !== randomSon);
+
+// console.log(`To‘g‘ri topdingiz! ${randomSon}`);
+
+
+// 8 misol
+
+// let son = 1;
+
+// while (son <= 50) {
+//     if (son % 3 === 0) {
+//         if (son === 21) {
+//             break; 
+//         }
+//         console.log(son);
+//     }
+
+//     son = son + 1; 
+// }
+
